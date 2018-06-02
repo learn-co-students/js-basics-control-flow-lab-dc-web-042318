c0c500062bea0630aa535ec84eba0720b6ec8cd9
codingkasia <codingkasia@gmail.com>
2018-06-02 16:40:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code in this file!
+function scuberGreetingForFeet (someValue) {
+  if (someValue <= 199) {
+    return 'This one is on me!'
+  } else if ((someValue > 2000) && (someValue <= 2500)) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (someValue > 2500) {
+    return 'No can do.'
+  } else {
+
+  }
+}
+function ternaryCheckCity (city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+    switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.'
+      break;
+  }
+  return response
+}
